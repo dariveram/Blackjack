9ab03161ebe875c7fa0fac520a3bbad56c4e5a53
David Rivera Ramírez <dariveram@hotmail.com>
2016-03-21 11:33:18 -0600

    Prueba 2do cambio.

--- app/src/main/java/com/cenfotec/proyectofinal/blackjack/MainActivity.java
+++ app/src/main/java/com/cenfotec/proyectofinal/blackjack/MainActivity.java
@@ -9,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //prueba 1er cambio.
-
     }
 }
