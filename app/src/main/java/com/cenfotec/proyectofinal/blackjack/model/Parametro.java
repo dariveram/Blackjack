e81882fc03361ea346475999e1c3cea67bff8af3
David Rivera Ramírez <dariveram@hotmail.com>
2016-04-02 08:43:42 -0600

    Se agrega clase Parametro.java.

---
+++ app/src/main/java/com/cenfotec/proyectofinal/blackjack/model/Parametro.java
@@ -0,0 +1,10 @@
+package com.cenfotec.proyectofinal.blackjack.model;
+
+/**
+ * Created by drivera on 2016-04-02.
+ */
+public class Parametro {
+    // TODO: Humberto, utiliza esta clase para administrar los parámetros.
+    // TODO: Por favor crea un método publico que se llame cargarParametros(), la idea es que ese método
+    // TODO: lea los valores del Internal Storage y los cargue en las variables privadas de la clase.
+}
